e934d8c5c2e6a27a80f2b8ab2ac04672989917b0
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-01-13 16:18:46 +0530

    url update

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 
-let MONGO_URL = "mongodb://localhost:27017/BookStore";
+// let MONGO_URL = "mongodb://localhost:27017/BookStore";
+let MONGO_URL = "mongodb+srv://root:root@booklibrary.lnxbe4n.mongodb.net/?retryWrites=true&w=majority";
 module.exports = { PORT : 8000, MONGO_URL }
